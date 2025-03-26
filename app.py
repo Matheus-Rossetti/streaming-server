864ed76764ad332055758ef7b39189f40d1a1b65
Matheus-Rossetti <matheuzdreher@gmail.com>
2025-03-26 11:10:03 -0300

    inital commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+import os
+
+from flask import send_from_directory
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Streaming de videos'
+
+
+@app.route('/video/<video_name>/<file_name>')
+def stream(video_name, file_name):
+    video_path = os.path.join('videos', video_name)
+    return send_from_directory(video_path, file_name)
+
+
+if __name__ == '__main__':
+    app.run()

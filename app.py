1d032cdbe1b7c15973e52c15b07f1067e1ac938d
Matheus-Rossetti <matheuzdreher@gmail.com>
2025-03-31 13:55:38 -0300

    .venv inicada | alguns TODOs adicionados | rota de upload adicionada sem parametro

--- app.py
+++ app.py
@@ -13,12 +13,27 @@ def hello_world():
 
 @app.route('/video/<video_name>/<file_name>')
 def stream(video_name, file_name):
+
+    #TODO criar link temporario usando a API do S3 e retornar o link ao app
+
     return jsonify(f'Seu vídeo estará em: /video/{video_name}/{file_name}')
 
 @app.route('/filmes-lista')
-def lista_de_filmes():
+def lista_de_videos():
+
+    # TODO requisição pro S3 para pegar todos os diretorios dentro do bucker
+    # TODO deve retornar uma lista, contar a lista e retornar a quantidade de filmes e seus nomes
+
     return jsonify('lista de todos os filmes:')
 
+@app.route('/upload')
+def upar_video():
+
+    # TODO verificar o tipo de arquivo
+    # TODO enviar arquivo para o micro-serviço com ffmpeg
+
+    # TODO ffmpeg vai guardar os arquivos gerados no S3
+    return
 
 if __name__ == '__main__':
     app.run()
